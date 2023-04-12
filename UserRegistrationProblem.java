cec8e6b3ff615be45ce1bbdfd95ff5448b935faa
gaurav459433 <gaurav459433@gmail.com>
2023-04-12 19:01:07 +0530

    Welcome To User Registration Program Using Lambda:

---
+++ UserRegistrationProblem.java
@@ -0,0 +1,7 @@
+package userregistration;
+
+public class UserRegistrationProblem {
+    public static void main(String[] args) {
+        System.out.println("Welcome To User Registration Program: ");
+    }
+}
